22c9a8402fda4272ef92eb2cbdf3b17511db71e7
juanmaferv96 <juanma.campanero96@gmail.com>
2025-12-04 20:35:09 +0100

    Actualizacion configs de seguridad por fallo al desplegar

--- src/main/java/com/vibeat/backend/config/SecurityConfig.java
+++ src/main/java/com/vibeat/backend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -13,6 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
